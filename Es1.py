f7d46aa51055cdf55501435e50e8c61b61ed8cce
GabrieleTomatis <gabriele.tomatis.58@itiscuneo.eu>
2024-09-13 19:30:57 +0200

    Comments

--- Es1.py
+++ Es1.py
@@ -1,4 +1,8 @@
+
+#creo lista1
 lista1 = [1, 2, 3, 4, 5]
+
+#all'interno della lista2, scrivo un for, che come parametri prende quelli della lista1, ma in modo che ogni parametro venga moltiplicato per due
 lista2 = [i * 2  for i in lista1]
 
 print(lista2)
