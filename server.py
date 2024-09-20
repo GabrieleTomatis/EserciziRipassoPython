08f1f5f01bee2f4b3f8d27e80ec2f32f4a5c4299
GabrieleTomatis <gabriele.tomatis.58@itiscuneo.eu>
2024-09-20 13:26:38 +0200

    modifics

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import socket as s
 
 server_address = ("192.168.65.103", 6980)
-BUFFER_SIZE = 4092 #uanti bit posso inviare o ricevere
+BUFFER_SIZE = 4092 
 
-udp_server_socket = s.socket(s.AF_INET, s.SOCK_DGRAM) #dgram è per udp stream è per tcp
+udp_server_socket = s.socket(s.AF_INET, s.SOCK_DGRAM) 
 udp_server_socket.bind(server_address)
 
-data, address = udp_server_socket.recvfrom(BUFFER_SIZE) #quando un client invia dei dati, vengono messi in data
+
+data, address = udp_server_socket.recvfrom(BUFFER_SIZE)
 print(f"Messaggio ricevuto: {data.decode('utf8')} da {address}")
