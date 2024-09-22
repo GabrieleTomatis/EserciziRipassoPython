338df2495c616798502f2a5b65756cdcbd7e8ae0
GabrieleTomatis <gabriele.tomatis.58@itiscuneo.eu>
2024-09-22 16:14:24 +0200

    Client

---
+++ Cliente2.py
@@ -0,0 +1,15 @@
+import socket as s
+
+udp_client_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+
+server_address = ("192.168.65.103", 6980) #ip server + porta
+BUFFER_SIZE = 4092 #grandezza buffer
+
+for i in range(10):
+    messaggio = "ciao!!" + str(i+1)
+    udp_client_socket.sendto(messaggio.encode("utf-8"), server_address)
+
+data, server_address = udp_client_socket.recvfrom(BUFFER_SIZE)
+
+print(f"Messaggior ricevuto da server: {data.decode()} da {server_address}")
+
