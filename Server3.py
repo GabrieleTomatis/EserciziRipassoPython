81f3082d7572c5292fb6a0c507337b9bfafa597d
GabrieleTomatis <gabriele.tomatis.58@itiscuneo.eu>
2024-09-22 16:46:54 +0200

    modifiche

--- Server3.py
+++ Server3.py
@@ -1,24 +1,27 @@
+#SERVER
 import socket as s
 
-server_address = ("192.168.1.51", 6980) #ip server + porta
-BUFFER_SIZE = 4092 #grandezza buffer
+server_address = ("192.168.1.51", 6980)
+BUFFER_SIZE = 4092
 
-udp_server_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)#uso ipv4 e udp
-udp_server_socket.bind(server_address)#sto associando l'oggetto che abbiamo creato all'ip e alla porta inizializzata sopra
+udp_server_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+udp_server_socket.bind(server_address)
+
+print("\nChat Client-Server: scrivi <arrivederci> per interrompere la connessione\n\n\n")
 
 while True:
     data, address = udp_server_socket.recvfrom(BUFFER_SIZE)
     print(f"{data.decode('utf-8')}, da {address}")
 
     if(data.decode('utf-8').lower() == "arrivederci"):
-        print("il cliente ha terminato la connessione")
+        print("Il client ha terminato la connessione...")
         break
 
-    risposta = input("rispondi al messaggio: ")
+    risposta = input("Rispondi al messaggio:  ")
     udp_server_socket.sendto(risposta.encode('utf-8'), address)
 
     if(risposta.lower() == "arrivederci"):
-        print("il server ha terminato la connessione")
+        print("Il server ha terminato la connessione...")
         break
 
 udp_server_socket.close()
